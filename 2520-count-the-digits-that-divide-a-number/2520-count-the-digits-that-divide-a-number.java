d3de6e73c65bcdbf7fd50217b0800c1629fa9af0
Kunal Chauhan <pc0128334@gmail.com>
2026-08-24 23:26:19 +0530

    Time: 0 ms (100%), Space: 42.3 MB (18.22%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigits(int num) {
+        int count=0;
+        int ori=num;
+        while(num>0){
+            int digit = num%10;
+            if(ori%digit==0){
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+    }
+}
