ccc67bc85564b99dee4933cb2171241619c91dee
Kunal Chauhan <pc0128334@gmail.com>
2026-09-05 20:39:38 +0530

    Time: 4 ms (84.95%), Space: 47.6 MB (5.71%) - LeetHub

---
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+
+        Arrays.sort(deck);
+
+        int gcd = 0;
+        int count = 1;
+
+        for (int i = 1; i < deck.length; i++) {
+
+            if (deck[i] == deck[i - 1]) {
+                count++;
+            } 
+            else {
+                gcd = findGCD(gcd, count);
+                count = 1;
+            }
+        }
+
+        // frequency of the last group
+        gcd = findGCD(gcd, count);
+
+        return gcd >= 2;
+    }
+
+    public int findGCD(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
